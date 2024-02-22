318c3f9d77c36aec8901af09b988bebce8248750
Md Mahbubur Rahman <MarufSAir@Users-MacBook-Air.local>
2024-02-22 15:33:09 -0400

    This commit adds the interview question solution of dsa with python course

---
+++ Exercises/Graph/graph.py
@@ -0,0 +1,55 @@
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+    
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ":", self.adj_list[vertex])
+
+    def add_vetex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+    
+    def add_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
+    
+    def remove_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except ValueError:
+                pass
+            return True
+        return False
+    
+    def remove_vertex(self, vertex):
+        if vertex in self.adj_list.keys():
+            for other_vertex in self.adj_list[vertex]:
+                self.adj_list[other_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
+
+my_grap = Graph()
+my_grap.add_vetex('A')
+my_grap.add_vetex('B')
+my_grap.add_vetex('C')
+my_grap.add_vetex('D')
+
+my_grap.add_edge('A', 'B')
+my_grap.add_edge('B', 'C')
+my_grap.add_edge('A', 'D')
+my_grap.add_edge('B', 'D')
+my_grap.add_edge('C', 'D')
+
+my_grap.print_graph()
+
+my_grap.remove_vertex('D')
+my_grap.print_graph()
